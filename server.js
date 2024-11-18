e3bf754f60c1de9b4c8931da0411bd1e65d542ea
nishtarw <nishta.sr04@gmail.com>
2024-11-18 00:27:45 -0500

    changes

--- server.js
+++ server.js
@@ -181,29 +181,18 @@ const songs = [
         title: "Faith",
         artist: "The Weeknd",
         review: "Giving this song a 8/10. This song by The Weeknd is definitely one of my favorites from the album...",
-        otherReviews: [
-            { user: "Louiss", review: "I love this album so much, I agree, I can literally feel his lyrics." },
-            { user: "Jes444", review: "5/10. I feel like it's kinda basic, I don't see what you guys are talking about." },
-        ],
         image: "/images/theweeknd3.jpg",
     },
     {
         title: "Normal Girl",
         artist: "SZA",
         review: "Giving this song a 7/10. I love the concept of the song because it feels relatable...",
-        otherReviews: [
-            { user: "Mayajs34", review: "10/10 for me. I've never heard something more relatable." },
-        ],
         image: "/images/sza.jpg",
     },
     {
         title: "Cry",
         artist: "Cigarettes After Sex",
         review: "10/10. This is my go-to crying song. It features the most beautiful instrumentation...",
-        otherReviews: [
-            { user: "mikeR334", review: "I cry to this song every day." },
-            { user: "burgers50", review: "1/10, all their songs sound the same." },
-        ],
         image: "/images/cas.jpg",
     },
 ];
